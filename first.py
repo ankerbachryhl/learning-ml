e19a382ce624821a8d665a8d5b4b922d3dd1ac54
ankerbachryhl <ankerbachryhl@gmail.com>
2016-12-17 16:15:14 +0100

    Made a iris classifier, using k-nearest neighbors :)

--- first.py
+++
@@ -1,6 +0,0 @@
-#Imports that are always required
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import mglearn
---
+++ iris_classifier/iris.py
@@ -0,0 +1,32 @@
+#Imports that are always required
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+iris_dataset = load_iris()
+
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+
+#Showing a pair plot of the Iris dataset
+
+#grr = pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+X_new = np.array([[7, 5.9, 5.1, 6.2]])
+print("X_new.shape: {}".format(X_new.shape))
+
+prediction = knn.predict(X_new)
+print("Prediction: {}".format(prediction))
+print("Predicted target name: {}".format(iris_dataset['target_names'][prediction]))
+
+#Test
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
